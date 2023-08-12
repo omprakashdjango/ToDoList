5a9d0a19eae30b8755e408edf8f6f1e64d015f78
omprakashdjango <omprakashdjango@gmail.com>
2023-08-12 15:35:02 +0530

    adding crud functionality

--- entry/urls.py
+++ entry/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.entry_list, name='entry_list'),
     path('single-entry/<str:pk>/', views.single_entry, name='single_entry'),
+    path('create-entry/', views.create_entry, name='create_entry'),
+    path('update-entry/<str:pk>/', views.update_entry, name='update_entry'),
+    path('delete-entry/<str:pk>', views.delete_entry, name='delete_entry'),
 ]
--- entry/views.py
+++ entry/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Entry
+from .forms import EntryForm
 
 # Create your views here.
 
@@ -14,3 +15,39 @@ def single_entry(request, pk):
 
     context = {'single_entry': single_entry_data}
     return render(request, 'entry/single_entry.html', context)
+
+# Create Entry To the To Do List
+def create_entry(request):
+    form = EntryForm()
+
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_list')
+        
+    context = {'form': form}
+    return render(request, 'entry/create_entry.html', context)
+
+# Update Entry To the To Do List
+def update_entry(request, pk):
+    entry_data = Entry.objects.get(id=pk)
+    form = EntryForm(instance=entry_data)
+
+    if request.method == 'POST':
+        form = EntryForm(request.POST, instance=entry_data)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_list')
+    
+    context = {'form': form}
+    return render(request, 'entry/create_entry.html', context)
+
+# Delete Entry To the To Do List
+def delete_entry(request, pk):
+    entry_data = Entry.objects.get(id=pk)
+    if request.method == 'POST':
+        entry_data.delete()
+        return redirect('entry_list')
+    context = {'entry': entry_data}
+    return render(request, 'entry/delete_template.html', context)
